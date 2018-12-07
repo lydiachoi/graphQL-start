5d562a32e9b5d493a7a43e86bf69a7da548d5272
lydia Choi <lydiachoi227@gmail.com>
2018-12-07 13:12:15 -0800

    created root query for initial query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,41 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString } = graphql; // grab objects from GQL package to use to define the objects below
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql; // grab objects from GQL package to use to define the objects below
 
+
+// Defining object types using GraphQLOjectType 
 const BookType = new GraphQLObjectType({
   name: "book", 
-  fields: function() {
-    ({id: {type: GraphQLString},
-      name: {type: GraphQLString},
-      genre: {type: GraphQLString}})
-  }
+  fields: function() {({        // wrapped inside of a function
+    id: {type: GraphQLString}, // must be a GraphQLString 
+    name: {type: GraphQLString},
+    genre: {type: GraphQLString}
+  })}
 });
 
-https://www.youtube.com/watch?v=A8vtRvz-lK0&list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f&index=7
+// Defining root query - how to initially jump into the graph from the front end 
+const RootQuery = new GraphQLObjectType({
+  name: "rootQueryType", 
+  fields: {       // different ways to "jump into the graph" -- starting points. 
+    book: {
+      type: BookType,
+      args: { id: {type: GraphQLString} }, 
+      resolve(parent, args) {
+        /// code to get data from db /other source
+      }
+    }
+  }
+})
+
+// creating a new schema; pass in options to define which queries clients are allowed to use 
+// exported here to be imported by app.js to use in the graphqlHTTP function
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
+/* 
+fields:() => ({
+  id: {type: GraphQLString},  
+  name: {type: GraphQLString},
+  genre: {type: GraphQLString}
+})
+*/ 
